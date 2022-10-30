d9253e87362a6d2f1d35c09f9f52ed776a5e19cc
jlewsey1 <107970839+jlewsey1@users.noreply.github.com>
2022-10-29 23:57:37 -0700

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by Jacob Lewsey on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
